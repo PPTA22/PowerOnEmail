57863ea42a3a5707e443cab98f661184b345964e
TPu7BL <40493117+TPu7BL@users.noreply.github.com>
2019-10-27 14:19:16 +0800

    Update sendEmail.py
    Add a sleep time before start, in case network not ready yet

--- sendEmail.py
+++ sendEmail.py
@@ -1,29 +1,30 @@
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-from email.mime.base import MIMEBase
-from email import encoders
-import datetime
-import socket
 
-hostname = socket.gethostname()
-ip_addrs = socket.gethostbyname_ex(socket.gethostname())[2]
+import datetime
+from time import sleep
+import socket
+sleep(25) # In case the router/ dhcp responde lately
+time = datetime.datetime.now()
 
 try:
+    hostname = socket.gethostname()
+    ip_addrs = socket.gethostbyname_ex(socket.gethostname())[2]
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.ehlo()
 
-    time = datetime.datetime.now()
+
     subject = f'{hostname}: {time.year} {time.month} {time.day}'
     # subject = MIMEText(subject, "text")
 
     body = f'Hostname: {hostname} \n'
-    body += f'Turn on time: {time.year} {time.month} {time.day} - {time.hour}:{time.minute}  \n'
+    body += f'Turn on time: {time.year} {time.month} {time.day} - {str(time.hour).zfill(2)}:{time.minute}  \n'
     body += f'ip address: {ip_addrs}'
     msg = MIMEMultipart()
     body = MIMEText(body, "plain")
-    email_user,email_password = 'example@gmail.com', 'your_password'
-    email_send = 'receiver@gmail.com'
+    email_user,email_password = 'example@email.com', 'your_pwd'
+    email_send = 'receiver_example@email.com'
 
     msg['From'] = email_user
     msg['To'] = email_send
@@ -46,6 +47,15 @@ except Exception as e:
     print(e)
     print ('Something went wrong...')
 
-
+# pyinstaller -F sendEmail.py
 # sc.exe create sendEmail binPath= "C:\sendEmail.exe"
 # sc.exe delete sendEmail
+# remeber to choose automatic start in service tab
+
+'''
+Wnat new function
+- use txt as .ini config file, easy to modified email, such info
+- send including the real ip, not only the NAT ip
+- if network down, hold the service, until internet goup
+
+'''
